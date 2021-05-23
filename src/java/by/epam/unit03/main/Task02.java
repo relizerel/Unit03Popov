4f60417a5f6f857ed05e04696868633fb7667866
relizerel <1590753Aa>
2021-05-23 15:08:05 +0200

    Added solution for task #2

--- src/java/by/epam/unit03/main/Task02.java
+++ src/java/by/epam/unit03/main/Task02.java
@@ -2,6 +2,19 @@ package by.epam.unit03.main;
 
 public class Task02 {
     public static void main(String[] args) {
+        Task02 tempObject = new Task02();
+        int startNumber = 0;
+        int endNumber = 99;
+        System.out.println(tempObject.function(startNumber, endNumber));
+    }
 
+    private int function(int start, int end){
+        int result = 0;
+        while (start <= end) {
+            result = result + start;
+//            System.out.println(result + " + " + start);
+            start++;
+        }
+        return result;
     }
 }
