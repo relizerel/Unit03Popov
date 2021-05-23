55f0b72639178a08535988045d0e5a46dd4a1169
relizerel <1590753Aa>
2021-05-23 16:38:35 +0200

    Added solution for task #4

--- src/java/by/epam/unit03/main/Task04.java
+++ src/java/by/epam/unit03/main/Task04.java
@@ -2,6 +2,17 @@ package by.epam.unit03.main;
 
 public class Task04 {
     public static void main(String[] args) {
+        int x = -5;
+        Task04 tempObject = new Task04();
+        tempObject.function(x);
+    }
 
+    private void function(double x){
+        System.out.println("    x     y   ");
+        while (x <= 5) {
+            double y = 5 - (x * x /2);
+            System.out.println("    " + x + "    " + y);
+            x = x + 0.5;
+        }
     }
 }
