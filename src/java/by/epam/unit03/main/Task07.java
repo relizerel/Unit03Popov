b116e0d4ab203ce816816ec7cd613e75393ede11
relizerel <1590753Aa>
2021-05-24 15:34:37 +0200

    Added solution for task #7

--- src/java/by/epam/unit03/main/Task07.java
+++ src/java/by/epam/unit03/main/Task07.java
@@ -1,7 +1,26 @@
 package by.epam.unit03.main;
 
+import java.util.*;
+
 public class Task07 {
     public static void main(String[] args) {
 
+        String romanNumber = "V";
+        Task07 tempObject = new Task07();
+        System.out.println(tempObject.converter(romanNumber));
     }
+
+    // Я так понимаю, что "цифра" - это от 0 до 9? тогда все просто:
+    private int converter(String romanNumber){
+        String[] romanStrings = {"Пустое значение", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        if (!Arrays.asList(romanStrings).contains(romanNumber)){
+            return 0;
+        }
+        return Arrays.asList(romanStrings).indexOf(romanNumber);
+    }
+    // либо можно сделать перебор массива через фор
+    // либо можно городить if/switch
+
+    // либо делать через Map`ы,
+    // это если нужно сделать для числа любого размера, либо для конвертации в обратную сторону
 }
